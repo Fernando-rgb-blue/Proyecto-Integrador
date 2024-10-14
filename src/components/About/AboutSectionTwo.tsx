27be5fc84410c73bbd1bbe6bb0342face6b43a39
nuevo1usuario <t052701120@unitru.edu.pe>
2024-10-14 02:03:05 -0500

    Código de la misión y visión
    Este código crea una sección que presenta visualmente dos columna tanto para la misión y visión con un título, una imagen relevante y un texto descriptivo.

--- src/components/About/AboutSectionTwo.tsx
+++ src/components/About/AboutSectionTwo.tsx
@@ -5,56 +5,67 @@ const AboutSectionTwo = () => {
     <section className="py-16 md:py-20 lg:py-28">
       <div className="container">
         <div className="-mx-4 flex flex-wrap items-center">
-          <div className="w-full px-4 lg:w-1/2">
-            <div
-              className="relative mx-auto mb-12 aspect-[25/24] max-w-[500px] text-center lg:m-0"
-              data-wow-delay=".15s"
-            >
-              <Image
-                src="/images/about/about-image-2.svg"
-                alt="about image"
-                fill
-                className="drop-shadow-three dark:hidden dark:drop-shadow-none"
-              />
-              <Image
-                src="/images/about/about-image-2-dark.svg"
-                alt="about image"
-                fill
-                className="hidden drop-shadow-three dark:block dark:drop-shadow-none"
-              />
-            </div>
-          </div>
-          <div className="w-full px-4 lg:w-1/2">
-            <div className="max-w-[470px]">
+
+          <div className="w-full px-4 lg:w-1/2 flex flex-col items-center">
+            <div className="max-w-[470px] text-center">
               <div className="mb-9">
-                <h3 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
-                  Bug free code
-                </h3>
+                <h1 className="mb-4 text-4xl font-bold text-black dark:text-white sm:text-5xl lg:text-3xl xl:text-5xl">
+                  Misión
+                </h1>
+                <div
+                  className="relative mb-12 aspect-[25/24] max-w-[200px] mx-auto"  //className="relative mx-auto mb-12 aspect-[25/24] max-w-[200px] text-center lg:m-0"
+                  data-wow-delay=".15s"
+                >
+                  <Image
+                    src="/images/about/14244339.svg"
+                    alt="about image"
+                    fill
+                    className="drop-shadow-three dark:hidden dark:drop-shadow-none"
+                  />
+                  <Image
+                    src="/images/about/14244339.svg"
+                    alt="about image"
+                    fill
+                    className="hidden drop-shadow-three dark:block dark:drop-shadow-none"
+                  />
+                </div>
                 <p className="text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
-              </div>
-              <div className="mb-9">
-                <h3 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
-                  Premier support
-                </h3>
-                <p className="text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt.
-                </p>
-              </div>
-              <div className="mb-1">
-                <h3 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
-                  Next.js
-                </h3>
-                <p className="text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed">
-                  Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt
-                  consectetur adipiscing elit setim.
+                  Formación académica de recursos humanos en computación a nivel de pregrado, con el objetivo de generar y difundir el conocimiento comprometido con los desafíos nacionales e internacionales, que llevan al avance científico y tecnológico de la computación. 
                 </p>
               </div>
             </div>
+          </div>  
+        
+          <div className="w-full px-4 lg:w-1/2 flex flex-col items-center">
+            <div className="max-w-[470px] text-center">
+              <div className="mb-9">
+                <h1 className="mb-4 text-4xl font-bold text-black dark:text-white sm:text-5xl lg:text-3xl xl:text-5xl">
+                  Visión
+                </h1>
+                <div
+                  className="relative mb-12 aspect-[25/24] max-w-[200px] mx-auto"  //className="relative mx-auto mb-12 aspect-[25/24] max-w-[200px] text-center lg:m-0"
+                  data-wow-delay=".15s"
+                >
+                  <Image
+                    src="/images/about/17234191.svg"
+                    alt="about image"
+                    fill
+                    className="drop-shadow-three dark:hidden dark:drop-shadow-none"
+                  />
+                  <Image
+                    src="/images/about/17234191.svg"
+                    alt="about image"
+                    fill
+                    className="hidden drop-shadow-three dark:block dark:drop-shadow-none"
+                  />
+                </div>
+                <p className="text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed">
+                  La Escuela Profesional de Informática será líder en la enseñanza nacional e investigación, preparando ciudadanos que puedan contribuir con la creatividad, humanidad e innovación para una sociedad global, justa y con éxito.
+                </p>
+              </div>
+            </div>
           </div>
+
         </div>
       </div>
     </section>
