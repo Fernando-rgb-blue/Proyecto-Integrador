d60ac010a53824d5e4acb748a4dff022d4fa7d79
nuevo1usuario <t052701120@unitru.edu.pe>
2024-10-14 01:52:52 -0500

    Actualización de página nosotros
    Se cambio el título y la descripción de la página "Nosotros".

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,8 +5,8 @@ import Breadcrumb from "@/components/Common/Breadcrumb";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "About Page | Free Next.js Template for Startup and SaaS",
-  description: "This is About Page for Startup Nextjs Template",
+  title: "Escuela de Informática | Nosotros",
+  description: "Esta es la página de Nosotros",
   // other metadata
 };
 
@@ -14,8 +14,8 @@ const AboutPage = () => {
   return (
     <>
       <Breadcrumb
-        pageName="About Page"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
+        pageName="Nosotros"
+        description="En la Escuela de Ingeniería Informática, nos dedicamos a transformar la pasión por la tecnología en habilidades concretas."
       />
       <AboutSectionOne />
       <AboutSectionTwo />
