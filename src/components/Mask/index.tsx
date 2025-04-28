3cfb18b65c828ce16eaa38e60f327c53dea46420
Fernando-rgb-blue <fernandoalfaro209@gmail.com>
2025-04-27 19:11:34 -0500

    Cambio xD

--- src/components/Mask/index.tsx
+++ src/components/Mask/index.tsx
@@ -2,7 +2,7 @@
 import React, { useRef } from 'react';
 
 const members = [
-  { name: 'Alfato Titto Anthony Fernando', email: 'TuZamakito1999@gmail.com' },
+  { name: 'Alfato Titto Anthony Fernando', email: 'fernandoalfaro209@gmail.com' },
   { name: 'Gonzales Matos Walter Manuel', email: 'manuelgonzmatos@gmail.com' },
   { name: 'Urcia Peláez Luis Alexander', email: 'urciapelaezluisalex@gmail.com' },
 ];
